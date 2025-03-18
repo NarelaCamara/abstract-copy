4aac16b5ca10ca73f23acc6ede2dac438fd79704
RoxanaCamara <roxana.camara07@gmail.com>
2025-03-18 16:24:24 -0300

    feat: componentes

---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { Search } from "./search/search";
+export { Navigation } from "./navigation/navigation";
---
+++ src/components/navigation/navigation.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+export const Navigation = () => {
+  return (
+    <div className="flex flex-row justify-between p-4">
+      <div>
+        <h4>Help Center</h4>
+      </div>
+
+      <div>
+        <form className="flex items-center max-w-sm mx-auto">
+          <label className="sr-only">Search</label>
+          <div className="relative w-full">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 18 20"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm12 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0V6a3 3 0 0 0-3-3H9m1.5-2-2 2 2 2"
+                />
+              </svg>
+            </div>
+            <input
+              type="text"
+              id="simple-search"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search branch name..."
+              required
+            />
+          </div>
+        </form>
+
+        <button
+          type="button"
+          className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+        >
+          Submit a request
+        </button>
+
+        <button
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          Sign in
+        </button>
+      </div>
+    </div>
+  );
+};
